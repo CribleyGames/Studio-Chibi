808068c0fadd4632e2581edccb9aa7fe85f55246
Cribley <57372026+CribleyGames@users.noreply.github.com>
2021-10-01 13:17:35 -0400

    i swear it is 100% cleaned up now

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function openNav() {
-   document.getElementById("mySidebar").style.width = "250px";
-   document.getElementById("main").style.marginLeft = "250px";
+	document.getElementById("mySidebar").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav() {
