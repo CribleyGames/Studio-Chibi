2571f41f7df6d09966be6fa20a68cdcc8655efe0
Cribley <57372026+CribleyGames@users.noreply.github.com>
2021-10-03 03:45:38 -0400

    bug fixes and multiple improvements to sidenav

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function openNav() {
-	document.getElementById("mySidebar").style.width = "250px";
+	document.getElementById("mySidenav").style.width = "250px";
 	document.getElementById("main").style.marginLeft = "250px";
+	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
-	document.getElementById("mySidebar").style.width = "0";
+	document.getElementById("mySidenav").style.width = "0";
 	document.getElementById("main").style.marginLeft = "0";
+	document.body.style.backgroundColor = "white";
 }
